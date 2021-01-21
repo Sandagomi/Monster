229bbfc8d06dad101a2c7af43388222b483edd43
Sandagomi <sandagomi@leafycode.com>
2021-01-21 14:58:42 +0530

    feat: completed search and filtering option.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import {CardList} from './components/card-list/card-list.component'
+import './App.css'
 
 
 const  App =()=> {
 
     const [monsters,setMonsters]= useState([])
+    const [search,setSearch]= useState('')
+
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(search.toString().toLowerCase()))
 
     useEffect(()=>{
         axios.get('https://jsonplaceholder.typicode.com/users').then(res=>res.data).then(users=>setMonsters(users));
     });
 
-
   return (
     <div className="App">
-        <CardList monster={monsters}/>
+        <input type='search' placeholder="search monster" onChange={e=> setSearch(e.target.value)}/>
+        <CardList monster={filteredMonsters}/>
     </div>
   );
 }
